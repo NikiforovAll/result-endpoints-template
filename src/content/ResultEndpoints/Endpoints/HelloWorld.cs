cb020b5e7522e6deeb029f70ff6ef5718b2b68bf
nikiforovall <alexey.nikiforovall@gmail.com>
2024-08-16 17:53:11 +0300

    add 0.2

--- src/content/ResultEndpoints/Endpoints/HelloWorld.cs
+++ src/content/ResultEndpoints/Endpoints/HelloWorld.cs
@@ -1,19 +1,29 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace ResultEndpoints.Endpoints;
 
-public class HelloWorld : EndpointBaseSync.WithRequest<string>.WithResult<Ok<string>>
+public class HelloWorld
+    : EndpointBaseSync.WithRequest<string>.WithResult<
+        Results<Ok<string>, BadRequest<ProblemDetails>>
+    >
 {
     [EndpointSummary("Says hello")]
     [EndpointDescription("Says hello based on the request")]
     [Tags("Hello")]
     [EndpointName(nameof(HelloWorld))]
     [HttpGet("/hello-world")]
-    public override Ok<string> Handle([FromQuery(Name = "q")] string request)
+    public override Results<Ok<string>, BadRequest<ProblemDetails>> Handle(
+        [FromQuery(Name = "q")] [Length(3, 100)] string request
+    )
     {
         if (request == "error")
         {
             throw new Exception("Something went wrong...");
+        }
+        else if (request == "badrequest")
+        {
+            return TypedResults.BadRequest(TypedResults.Problem("Something bad happened").ProblemDetails);
         }
 
         return TypedResults.Ok($"Hello, {request}");
